f614c898f30fcebfbb954df0f4d27f29a9be0e76
akbarmadaminov <mr.inovak@gmail.com>
2022-06-20 01:45:30 +0300

    Assignment 4

---
+++ Assets/ProxyMonster.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ProxyMonster : MonoBehaviour, MonsterAttack
+    {
+        private RealMonster _realMonster;
+        public float Distance;
+        public string name = "ProxyMonsterX";
+
+        private void Awake()
+        {
+            _realMonster = gameObject.AddComponent<RealMonster>();
+            Attack(name);
+        }
+
+        public void Attack(string name)
+        {
+            if (Distance <= 10f)
+                _realMonster.Attack(name);
+            else
+                Debug.Log("It is too far to attack for " + name);
+        }
+    }
+}
